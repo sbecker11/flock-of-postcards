3d7925097916c58a5ab8df9d3493c580e7b641f2
Shawn Becker <sbecker11@me.com>
2024-07-28 17:28:33 -0600

    moved pdf-resume-parser code into its own repo

--- static_content/jobs/first-run.sh
+++ static_content/jobs/first-run.sh
@@ -1,5 +1,22 @@
-#!/bin/bash
+#!/bin/zsh
 python -m venv venv
 source venv/bin/activate
-pip install -r requirements.txt
+
+# Function to add directory to PATH if not already present
+add-dir-to-path() {
+    local DIR_TO_ADD=$(realpath "$1")
+    if [[ ":$PATH:" != *":$DIR_TO_ADD:"* ]]; then
+        export PATH="$DIR_TO_ADD:$PATH"
+    fi
+}
+
+# Add directory to PATH
+add-dir-to-path resume-parser
+
+# Install required packages
+pip install -r requirements.txt -q
+
+# Update obs.mjs from jobs.xlsx
+echo "obs.mjs is being updated from jobs.xlsx"
 python xlsx2mjs.py
+echo "obs.mjs has been updated from jobs.xlsx"
